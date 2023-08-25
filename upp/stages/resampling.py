86eeea8eb46fd4ac362dde0ec3f622be63d2f82d
Ivan Oleksiyuk <ivan.oleksiyuk@gmail.com>
2023-08-25 19:52:59 +0200

    black

--- upp/stages/resampling.py
+++ upp/stages/resampling.py
@@ -37,16 +37,12 @@ def subdivide_bins(bins, n=2):
     return np.concatenate(comb_list)
 
 
-def upscale_array(
-    array, upscl, order=3, mode="nearest", nornalise=False, positive=True
-):
+def upscale_array(array, upscl, order=3, mode="nearest", nornalise=False, positive=True):
     # upscl must be integer
     xs = []
     for d in array.shape:
         n_bins = d
-        points = np.linspace(
-            -0.5 + 1 / 2 / upscl, n_bins - 0.5 - 1 / 2 / upscl, n_bins * upscl
-        )
+        points = np.linspace(-0.5 + 1 / 2 / upscl, n_bins - 0.5 - 1 / 2 / upscl, n_bins * upscl)
         xs.append(points)
 
     # return the smoothed array
@@ -75,14 +71,10 @@ def upscale_array_regionally(
         slice_obj_up = []
         for j in range(d):
             slice_obj.append(slice(starts_grid[j][i], finishes_grid[j][i]))
-            slice_obj_up.append(
-                slice(starts_grid[j][i] * upscl, finishes_grid[j][i] * upscl)
-            )
+            slice_obj_up.append(slice(starts_grid[j][i] * upscl, finishes_grid[j][i] * upscl))
         slice_obj = tuple(slice_obj)
         slice_obj_up = tuple(slice_obj_up)
-        up_array[slice_obj_up] = upscale_array(
-            array[slice_obj], upscl, order=order, mode=mode
-        )
+        up_array[slice_obj_up] = upscale_array(array[slice_obj], upscl, order=order, mode=mode)
     if normalise:
         up_array = up_array / up_array.sum()
     return up_array
@@ -124,9 +116,7 @@ class Resampling:
         num_jets = int(len(jets) * component.sampling_fraction)
         target_pdf = self.target.hist.pbin
         target_hist = target_pdf * num_jets
-        target_hist = (
-            np.floor(target_hist + self.rng.random(target_pdf.shape))
-        ).astype(int)
+        target_hist = (np.floor(target_hist + self.rng.random(target_pdf.shape))).astype(int)
 
         _hist, binnumbers = bin_jets(jets[self.config.vars], self.config.flat_bins)
         assert target_pdf.shape == _hist.shape
@@ -134,13 +124,9 @@ class Resampling:
         # loop over bins and select relevant jets
         all_idx = []
         for bin_id in np.ndindex(*target_hist.shape):
-            idx = np.where((bin_id == binnumbers.T).all(axis=-1))[0][
-                : target_hist[bin_id]
-            ]
+            idx = np.where((bin_id == binnumbers.T).all(axis=-1))[0][: target_hist[bin_id]]
             if len(idx) and len(idx) < target_hist[bin_id]:
-                idx = np.concatenate(
-                    [idx, self.rng.choice(idx, target_hist[bin_id] - len(idx))]
-                )
+                idx = np.concatenate([idx, self.rng.choice(idx, target_hist[bin_id] - len(idx))])
             all_idx.append(idx)
         idx = np.concatenate(all_idx).astype(int)
         if len(idx) < num_jets:
@@ -151,9 +137,7 @@ class Resampling:
     def pdf_select_func(self, jets, component):
         # bin jets
         if self.upscale_pdf > 1:
-            bins = [
-                subdivide_bins(bins, self.upscale_pdf) for bins in self.config.flat_bins
-            ]
+            bins = [subdivide_bins(bins, self.upscale_pdf) for bins in self.config.flat_bins]
         else:
             bins = self.config.flat_bins
 
@@ -166,9 +150,7 @@ class Resampling:
         num_samples = int(len(jets) * component.sampling_fraction)
         ratios = safe_divide(self.target.hist.pbin, component.hist.pbin)
         if self.upscale_pdf > 1:
-            ratios = upscale_array_regionally(
-                ratios, self.upscale_pdf, self.regionlengthsd
-            )
+            ratios = upscale_array_regionally(ratios, self.upscale_pdf, self.regionlengthsd)
         probs = ratios[binnumbers]
         idx = random.choices(np.arange(len(jets)), weights=probs, k=num_samples)
         return idx
@@ -177,9 +159,7 @@ class Resampling:
         unique, ups_counts = np.unique(idx, return_counts=True)
         component._unique_jets += len(unique)
         max_ups = ups_counts.max()
-        component._ups_max = (
-            max_ups if max_ups > component._ups_max else component._ups_max
-        )
+        component._ups_max = max_ups if max_ups > component._ups_max else component._ups_max
 
     def sample(self, components, stream, progress):
         # loop through input file
@@ -225,9 +205,7 @@ class Resampling:
 
         for c in components:
             if not c._complete:
-                raise ValueError(
-                    f"Ran out of {c} jets after writing {c.writer.num_written:,}"
-                )
+                raise ValueError(f"Ran out of {c} jets after writing {c.writer.num_written:,}")
 
     def run_on_region(self, components, region):
         # compute the target pdf
@@ -240,9 +218,7 @@ class Resampling:
             # make sure all tags equal_jets are the same
             equal_jets_flags = [c.equal_jets for c in cs]
             if len(set(equal_jets_flags)) != 1:
-                raise ValueError(
-                    "equal_jets must be the same for all components in a sample"
-                )
+                raise ValueError("equal_jets must be the same for all components in a sample")
             equal_jets_flag = equal_jets_flags[0]
 
             # setup input stream
@@ -280,14 +256,8 @@ class Resampling:
             )
 
     def set_component_sampling_fractions(self):
-        if (
-            self.config.sampling_fraction == "auto"
-            or self.config.sampling_fraction is None
-        ):
-            log.info(
-                "[bold green]Sampling fraction chosen for each component"
-                " automatically..."
-            )
+        if self.config.sampling_fraction == "auto" or self.config.sampling_fraction is None:
+            log.info("[bold green]Sampling fraction chosen for each component automatically...")
             for c in self.components:
                 if c.is_target(self.config.target):
                     c.sampling_fraction = 1
@@ -343,9 +313,6 @@ class Resampling:
 
         # finalise
         unique = sum(c.writer.get_attr("unique_jets") for c in self.components)
-        log.info(
-            "[bold green]Finished resampling a total of"
-            f" {self.components.num_jets:,} jets!"
-        )
+        log.info(f"[bold green]Finished resampling a total of {self.components.num_jets:,} jets!")
         log.info(f"[bold green]Estimated unqiue jets: {unique:,.0f}")
         log.info(f"[bold green]Saved to {self.components.out_dir}/")
